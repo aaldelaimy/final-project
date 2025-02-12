2abd3374bd6834f051ad1186e5f33fb5c46045ed
aaldelaimy <awaldelaimy@gmail.com>
2025-02-11 17:21:15 -0800

    fixes

--- app/main.py
+++ app/main.py
@@ -52,13 +52,17 @@ async def get_sensor_data(
     query = f"SELECT * FROM {sensor_type} WHERE 1=1"
     if start_date:
         try:
-            formatted_start = datetime.fromisoformat(start_date.replace('T', ' ')).strftime('%Y-%m-%d %H:%M:%S')
+            formatted_start = start_date
+            if 'T' in start_date:
+                formatted_start = start_date.replace('T', ' ')
             query += f" AND timestamp >= '{formatted_start}'"
         except ValueError:
             raise HTTPException(status_code=400, detail="Invalid start date format")
     if end_date:
         try:
-            formatted_end = datetime.fromisoformat(end_date.replace('T', ' ')).strftime('%Y-%m-%d %H:%M:%S')
+            formatted_end = end_date
+            if 'T' in end_date:
+                formatted_end = end_date.replace('T', ' ')
             query += f" AND timestamp <= '{formatted_end}'"
         except ValueError:
             raise HTTPException(status_code=400, detail="Invalid end date format")
